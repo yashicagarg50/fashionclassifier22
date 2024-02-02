196f39d8150bab0c4a54599c06fe9dd8c780e161
Yashica Garg <yashicagarg50@gmail.com>
2024-02-03 04:44:21 +0530

    add

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ def classify_image(img):
     pred, idx, probs = learn.predict(img)
     return dict(zip(categories,map (float,probs)))
 
-image = gr.inputs. Image (shape= (192, 192))
-label = gr.outputs. Label()
+image = gr.inputs.Image (shape= (192, 192))
+label = gr.outputs.Label()
 # examples = ['/kaggle/input/ip-pics/goth.jpg','/kaggle/input/bizzzzzzzzzz/bizzzzzzz.jpg','/kaggle/input/piccccc/ethnic.jpg' ]
 intf = gr. Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
 intf.launch(inline=False)
