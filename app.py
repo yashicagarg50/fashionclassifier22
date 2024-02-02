c7e08ed5d001737b291c084e607a523a3932bbb0
Yashica Garg <yashicagarg50@gmail.com>
2024-02-03 04:14:30 +0530

    Add

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
+from fastai.vision.all import *
+import gradio as gr
+def style(x): return x[0].isupper()
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+
+learn=load_learner('https://drive.google.com/file/d/1jjGAKMHxvL7xJIHXFXLBULOmln9eoavW/view?usp=sharing')
+
+categories = ('active wear','bussiness wear','ethnic wear','goth fashion')
+def classify_image(img):
+    
+    pred, idx, probs = learn.predict(img)
+    return dict(zip(categories,map (float,probs)))
+
+image = gr.inputs. Image (shape= (192, 192))
+label = gr.outputs. Label()
+# examples = ['/kaggle/input/ip-pics/goth.jpg','/kaggle/input/bizzzzzzzzzz/bizzzzzzz.jpg','/kaggle/input/piccccc/ethnic.jpg' ]
+intf = gr. Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
+intf.launch(inline=False)
