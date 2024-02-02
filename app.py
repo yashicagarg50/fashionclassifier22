76d662f1c313e905a6ad303c90a5efaa04e726fb
Yashica Garg <yashicagarg50@gmail.com>
2024-02-03 05:02:36 +0530

    add

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ import gradio as gr
 def style(x): return x[0].isupper()
 
 
-learn=load_learner('model.pkl')
+learn = load_learner('model.pkl')
 
 categories = ('active wear','bussiness wear','ethnic wear','goth fashion')
+
 def classify_image(img):
-    
     pred, idx, probs = learn.predict(img)
     return dict(zip(categories,map (float,probs)))
 
 image = gr.inputs.Image(shape=(192, 192))
 label = gr.outputs.Label()
-# examples = ['/kaggle/input/ip-pics/goth.jpg','/kaggle/input/bizzzzzzzzzz/bizzzzzzz.jpg','/kaggle/input/piccccc/ethnic.jpg' ]
+examples = ['business.jpg','goth.jpeg']
 intf = gr. Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
 intf.launch(inline=False)
