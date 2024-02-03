cd4c708daf72257da59a3864d794f80c9f84d1c6
Yashica Garg <yashicagarg50@gmail.com>
2024-02-03 05:30:57 +0530

    add

--- app.py
+++ app.py
@@ -15,10 +15,16 @@ def classify_image(img):
     pred, idx, probs = learn.predict(img)
     return dict(zip(categories,map (float,probs)))
 
-image = gr.inputs.Image(shape=(192, 192))
-label = gr.outputs.Label()
+# image = gr.inputs.Image(shape=(192, 192))
+# label = gr.outputs.Label()
 
 # intf = gr. Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
 
-intf = gr.Interface(fn=classify_image,inputs=gr.Image(type="pil"),outputs=gr.Label(),examples=['business.jpg','goth.jpeg'])
+intf = gr.Interface(
+    fn=classify_image,
+    inputs=gr.Image(shape=(192, 192), type="pil"),
+    outputs=gr.Label(),
+    examples=['business.jpg', 'goth.jpeg']
+)
+
 intf.launch(inline=False)
